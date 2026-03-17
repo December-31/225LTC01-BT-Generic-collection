76f7c2fd055f6a93f3aa770bd167771619e2870b
December-31 <vancuong31122k6@gmail.com>
2026-03-18 00:01:55 +0700

    add bai4

---
+++ bai4/bai4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            var DS = new List<int> { 3,56,2,6,1 };
+            int count = 0;
+            foreach (int i in DS) 
+            { 
+                if(i % 2 == 0)
+                    count++;
+            }
+            Console.WriteLine("So luong so chan trong DS: " + count);
+        }
+    }
+}

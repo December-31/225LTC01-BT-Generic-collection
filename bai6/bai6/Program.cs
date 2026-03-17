b8e205d160f8749a9a868fa31824b7c725122dad
December-31 <vancuong31122k6@gmail.com>
2026-03-18 00:16:55 +0700

    add bai6

---
+++ bai6/bai6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            var DS = new List<string> { "An", "cuong", "huy", "minh", "hoa" };
+            Console.WriteLine("Danh sach chuoi: " + String.Join(" ,", DS));
+            Console.Write("Nhap chuoi can tim: ");
+            string name = Console.ReadLine();
+            if (DS.Contains(name))
+            {          
+                Console.WriteLine($"Co chuoi {name} trong danh sach");
+            }
+            else
+            {
+                Console.WriteLine($"Khong co chuoi {name} trong danh sach");
+            }
+        }
+    }
+}

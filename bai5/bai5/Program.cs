0d6afa7b904caf741728175e7c44e06df824aaf1
December-31 <vancuong31122k6@gmail.com>
2026-03-18 00:11:28 +0700

    add bai5

---
+++ bai5/bai5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            var DS = new List<string> { "An", "cuong", "huy", "minh", "hoa"};
+            Console.WriteLine("Danh sach sinh vien: "+ String.Join(" ,",DS));
+            Console.Write("Nhap ten sinh vien can xoa: ");
+            string name = Console.ReadLine();
+            if(DS.Contains(name))
+            {
+                DS.Remove(name);
+                Console.WriteLine("Danh sach sinh vien sau khi xoa: " + String.Join(" ,", DS));
+            }
+            else
+            {
+                Console.WriteLine("Khong tim thay sinh vien can xoa");
+            }
+        }
+    }
+}
---
+++ bai5/bai5/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("bai5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bai5")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f0e3d714-ee88-40de-a9d0-9c3b14cf255a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

9ffdbc13cbc768771a1e2be592e89468d883d106
December-31 <vancuong31122k6@gmail.com>
2026-03-17 23:14:22 +0700

    add bai1

---
+++ bai1/bai1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            var DS = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"nhap phan tu {i+1}:");
+                int gt=int.Parse(Console.ReadLine());
+                DS.Add(gt);
+            }
+            Console.WriteLine("cac phan tu: "+string.Join(",", DS));   
+        }
+    }
+}

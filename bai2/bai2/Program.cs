f0d9fa1edb33bad2b3a0d05ebdae497f434490b8
December-31 <vancuong31122k6@gmail.com>
2026-03-17 23:25:21 +0700

    add bai2

---
+++ bai2/bai2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            var DS = new List<int> { 4,6,6,9,8};
+            int tong = DS.Sum();
+            Console.WriteLine("Tong: " + tong);
+
+        }
+    }
+}

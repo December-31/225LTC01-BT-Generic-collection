68d7ad529b741034982da94b04e0cf0c5bd4c7a3
December-31 <vancuong31122k6@gmail.com>
2026-03-18 01:25:39 +0700

    add bai9

---
+++ bai9/bai9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            var DS = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"nhap phan tu {i + 1}:");
+                int gt = int.Parse(Console.ReadLine());
+                DS.Add(gt);
+            }
+            Console.WriteLine("Danh sach: " + string.Join(",", DS));
+            for (int i = 0; i < DS.Count; i++)
+            {
+                for (int j = i + 1; j < DS.Count; j++)
+                {
+                    if (DS[i] == DS[j])
+                    {
+                        DS.RemoveAt(i);
+                    }
+                }
+            }
+            Console.WriteLine("Danh sach: " + string.Join(",", DS));
+        }
+    }
+}

9201765bcb4ede56d1e8b7d5c46790ba0019efb1
December-31 <vancuong31122k6@gmail.com>
2026-03-17 23:57:56 +0700

    add bai3

---
+++ bai3/bai3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            var DS = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"nhap phan tu {i + 1}:");
+                int gt = int.Parse(Console.ReadLine());
+                DS.Add(gt);
+            }
+            int max=DS.Max();
+            Console.WriteLine("So lon nhat: "+max);
+        }
+    }
+}

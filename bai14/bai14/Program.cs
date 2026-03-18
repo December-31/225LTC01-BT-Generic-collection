81b65a25a03abfcb27318c8d285df5d0cb0c585b
December-31 <vancuong31122k6@gmail.com>
2026-03-18 21:52:33 +0700

    adđ bai14

---
+++ bai14/bai14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            List<Student> students = new List<Student> {
+            new Student(1, "Cuong"),
+            new Student(2, "Lan"),
+            new Student(3, "Minh")
+        };
+            foreach (var s in students) Console.WriteLine($"ID: {s.Id}, Tên: {s.Name}");
+            Console.Write("\nNhap ID can xoa: ");
+            int idToRemove = int.Parse(Console.ReadLine());
+            students.RemoveAll(s => s.Id == idToRemove);
+            Console.WriteLine("Đa cap nhat danh sach.");
+            foreach (var s in students) Console.WriteLine($"ID: {s.Id}, Tên: {s.Name}");
+        }
+    }
+}

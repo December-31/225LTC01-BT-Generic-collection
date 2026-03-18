58a04203f6d7ed95819fbe0420910bcd4eb75c77
December-31 <vancuong31122k6@gmail.com>
2026-03-18 22:06:45 +0700

    add bai18

---
+++ bai18/bai18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            List<Student> list = new List<Student> {
+            new Student { Name = "Cuong", Score = 9.5 },
+            new Student { Name = "An", Score = 8.0 }
+            };
+
+            var topStudent = list.OrderByDescending(s => s.Score).FirstOrDefault();
+            if (topStudent != null)
+                Console.WriteLine($"SV điem cao nhat: {topStudent.Name} ({topStudent.Score})");
+        }
+    }
+}
---
+++ bai18/bai18/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai18
+{
+    internal class Student
+    {
+        public string Name;
+        public double Score;
+    }
+}

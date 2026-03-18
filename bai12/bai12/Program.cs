7590dc5822e09bd1a93feb31ed1fd8bc078452b6
December-31 <vancuong31122k6@gmail.com>
2026-03-18 21:37:03 +0700

    add bai12

---
+++ bai12/bai12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            List<Student> students = new List<Student> {
+            new Student(1, "Cuong"),
+            new Student(2, "Lan"),
+            new Student(3, "Minh")
+            };
+            Console.Write("\nNhap ten can tim: ");
+            string searchName = Console.ReadLine();
+            var found = students.Where(s => s.Name.ToLower().Contains(searchName.ToLower())).ToList();
+            Console.WriteLine(found.Count > 0 ? $"Tim thay {found.Count} SV." : "Khong tim thay.");
+        }
+    }
+}

05e33869a7e21b762f95086c551bf63941c41ea7
December-31 <vancuong31122k6@gmail.com>
2026-03-18 21:31:13 +0700

    add bai11

---
+++ bai11/bai11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            List<Student> students = new List<Student> {
+            new Student(1, "Cuong"),
+            new Student(2, "Lan"),
+            new Student(3, "Minh")
+        };
+
+            Console.WriteLine("--- Danh sách sinh viên ---");
+            foreach (var s in students) 
+                Console.WriteLine($"ID: {s.Id}, Tên: {s.Name}");
+        }
+    }
+}
---
+++ bai11/bai11/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai11
+{
+    internal class Student
+    {
+        public int Id;
+        public string Name;
+
+        public Student(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}

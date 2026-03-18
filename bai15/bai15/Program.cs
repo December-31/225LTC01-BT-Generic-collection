17309e22ffe05478f981cdb56490ad374b7640b8
December-31 <vancuong31122k6@gmail.com>
2026-03-18 21:55:11 +0700

    add bai15

---
+++ bai15/bai15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            Dictionary<string, string> dict = new Dictionary<string, string> {
+            { "hello", "xin chào" },
+            { "student", "sinh viên" },
+            { "computer", "máy tính" }
+             };
+
+            Console.Write("Nhap tu tieng Anh: ");
+            string word = Console.ReadLine().ToLower();
+
+            if (dict.ContainsKey(word))
+                Console.WriteLine($"Nghia la: {dict[word]}");
+            else
+                Console.WriteLine("Tu nay chua co trong tu đien.");
+        }
+    }
+}

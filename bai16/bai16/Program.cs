42c313b39d7d17a97d7e9811e4065fa93e8d8688
December-31 <vancuong31122k6@gmail.com>
2026-03-18 21:58:07 +0700

    add bai16

---
+++ bai16/bai16/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            Console.Write("Nhap chuoi: ");
+            string input = Console.ReadLine();
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+
+            foreach (char c in input)
+            {
+                if (counts.ContainsKey(c)) counts[c]++;
+                else counts[c] = 1;
+            }
+
+            foreach (var item in counts)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+        }
+    }
+}

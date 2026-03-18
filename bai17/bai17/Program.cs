a6a73b1d0e6f48762f816a2eff6198fdafa64469
December-31 <vancuong31122k6@gmail.com>
2026-03-18 22:01:48 +0700

    add bai17

---
+++ bai17/bai17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            List<int> nums = new List<int>  ();
+            Console.Write("Nhap so luong phan tu: ");
+            int n= int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Nhap phan tu thu {i + 1}: ");
+                int num = int.Parse(Console.ReadLine());
+                nums.Add(num);
+            }
+            var mostFrequent = nums.GroupBy(x => x)
+                                   .OrderByDescending(g => g.Count())
+                                   .First().Key;
+
+            Console.WriteLine($"So xuat hien nhieu nhat: {mostFrequent}");
+        }
+    }
+}
